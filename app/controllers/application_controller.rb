7aed1246290fe5098b37cac86c73a9b76233aa59
isbs1990 <74824334+isbs1990@users.noreply.github.com>
2021-05-05 14:46:52 +0900

    アプリ完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :basic_auth
+  before_action :basic_auth
   before_action :authenticate_user!, except: [:show, :index]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -9,9 +9,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname , :email , :knj_first_name , :knj_last_name , :kana_first_name , :kana_last_name , :birth_day])
   end
 
-  # def basic_auth
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == 'admin' && password == '2222'
-  #   end
-  # end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
 end
